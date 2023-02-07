66372a7a3edeec0dfa833dea4505ae7e5b700a97
unknown <ramnath.ananth@gmail.com>
2023-02-07 09:56:59 -0500

    Data Upload apis

---
+++ src/routes/routes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const csvController = require("../controllers/controller/csv.controller");
+const upload = require("../middlewares/upload");
+
+let routes = (app) => {
+  router.post("/upload", upload.single("file"), csvController.upload);
+  router.get("/download", csvController.download);
+
+  app.use("/api/csv", router);
+};
+
+module.exports = routes;
